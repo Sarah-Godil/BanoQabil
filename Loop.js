ee027c5b9666d8175490c24bde2e92f79b2af0ed
Sarah-Godil <sarahgodil07@gmail.com>
2025-06-12 23:18:33 +0500

    Loop.js

---
+++ Loop.js
@@ -0,0 +1,22 @@
+let count = 1;
+let word = "Pakistan";
+
+do {
+  console.log("Do-while loop count:", word);
+  count++;
+} while (count <= 3);
+
+let word = "China";
+
+for (let count = 1; count <= 3; count++) {
+  console.log(`For loop count ${count}:`, word);
+}
+
+let count = 1;
+let word = "Pakistan";
+
+while (count <= 3) {
+  console.log(`While loop count ${count}:`, word);
+  count++;
+}
+
